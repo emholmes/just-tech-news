0ad40c350ba054c8b4e11b3174e98b3d4fa85f6f
Erin Holmes <e.holmes09@yahoo.com>
2021-08-05 15:08:13 -0500

    add front end logic to user login forms

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const routes = require("./controllers");
+const routes = require("./controllers/");
 const sequelize = require("./config/connection");
 const path = require("path");
 const exphbs = require("express-handlebars");
@@ -13,6 +13,7 @@ app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// middleware for handlebars
 app.use(express.static(path.join(__dirname, "public")));
 
 // turn on routes
