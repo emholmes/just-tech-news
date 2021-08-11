d2c5f9d2d71e467123059e2890967c173f7ca09f
Erin Holmes <e.holmes09@yahoo.com>
2021-08-11 18:30:26 -0500

    update secret to not use .env variable

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-  secret: process.env.COOKIE_SECRET,
+  secret: 'supersecretcookiesecret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
