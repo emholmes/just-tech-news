a2394dd5b8789aefac2738d3ed9152dca3ee2354
Erin Holmes <e.holmes09@yahoo.com>
2021-08-11 17:42:42 -0500

    fix config file

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const routes = require("./controllers/");
-const sequelize = require("./config/connection");
 const path = require("path");
 const exphbs = require("express-handlebars");
 const session = require("express-session");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-const helpers = require("./utils/helpers");
-const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
   secret: process.env.COOKIE_SECRET,
@@ -21,6 +20,12 @@ const sess = {
   })
 };
 
+app.use(session(sess));
+
+const helpers = require("./utils/helpers");
+
+const hbs = exphbs.create({ helpers });
+
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
@@ -28,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // middleware for handlebars
 app.use(express.static(path.join(__dirname, "public")));
-app.use(session(sess));
+
 
 // turn on routes
 app.use(routes);
